63e9a1151066ad820263c8d08f898969d7896306
TomSpencerLondon <tom@homeflow.co.uk>
2018-10-26 09:00:32 +0100

    Add form for uploading new songs to graphql

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,13 +3,21 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
-  render(){
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+  }
+  render() {    
+    if(this.props.data.loading) { return <div>Loading...</div>; }
 
-    console.log(this.props);
-    
-    return (<div>
-      SongList
-    </div>
+    return (<ul className="collection">
+      {this.renderSongs()}
+    </ul>
     );
   }
 }
@@ -17,6 +25,7 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
