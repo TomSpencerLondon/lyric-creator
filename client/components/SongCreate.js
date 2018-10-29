0b674a5ccdcc9b1886aee66f16bdefdc269f8d71
TomSpencerLondon <tom@homeflow.co.uk>
2018-10-29 09:03:03 +0000

    Add Likes and lyric adding for songs

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 
 class SongCreate extends Component {
@@ -14,15 +16,15 @@ class SongCreate extends Component {
     event.preventDefault();
 
     this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    });
+      variables: { title: this.state.title},
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'))
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
@@ -42,7 +44,6 @@ const mutation = gql`
       title
     }
   }
-
 `;
 
 
